6d23e42a848653c3fd2cd255669b8977d8e867da
Theo Mayer <tmayer2k@gmail.com>
2023-10-28 20:41:24 +0000

    routes.py skeleton

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,29 @@
 from app.models import User, Profile, Ride, RidePassenger, Message, Rating, Review, Post, Announcement
+
+@app.route('/') # landing
+# user registration and log in
+
+# example data insertion
+#   user = User(username='john_doe', email='john@example.com')
+#   db.session.add(user)
+#   db.session.commit()
+
+@app.route('/home')
+
+@app.route('/create_profile')
+
+@app.route('/home_admin')
+
+@app.route('/create_announcement')
+
+@app.route('/start_ride')
+
+@app.route('/start_ride/offer')
+
+@app.route('/start_ride/request')
+
+@app.route('/find_ride')
+
+@app.route('/view_profile')
+
+@app.route('/view_post')
