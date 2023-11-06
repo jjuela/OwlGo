ee11ba4d05000eb77c6938e4e7952aa87f124643
tmayer0 <mayert2@southernct.edu>
2023-11-06 15:33:07 -0500

    form/js update

--- app/static/js/start_ride_offer.js
+++ app/static/js/start_ride_offer.js
@@ -1,5 +1,23 @@
-document.getElementById('ridetype').addEventListener('change', function() {
+window.onload = function() {
     // hide fields first
+    hideAllFields();
+
+    document.getElementById('ridetype').addEventListener('change', function() {
+        // hide fields first
+        hideAllFields();
+
+        // show on ride type
+        if (this.value == '1') { // commute
+            showCommuteFields();
+        } else if (this.value == '2') { // errand
+            showErrandFields();
+        } else if (this.value == '3') { // leisure
+            showLeisureFields();
+        }
+    });
+}
+
+function hideAllFields() {
     document.getElementById('departingFrom-field').style.display = 'none';
     document.getElementById('departingAt-field').style.display = 'none';
     document.getElementById('destination-field').style.display = 'none';
@@ -9,30 +27,33 @@ document.getElementById('ridetype').addEventListener('change', function() {
     document.getElementById('reccuring-field').style.display = 'none';
     document.getElementById('accessibility-field').style.display = 'none';
     document.getElementById('description-field').style.display = 'none';
+}
 
-    // show on ride type
-    if (this.value == '1') { // commute
-        document.getElementById('departingFrom-field').style.display = 'block';
-        document.getElementById('departingAt-field').style.display = 'block';
-        document.getElementById('destination-field').style.display = 'block';
-        document.getElementById('arrival-field').style.display = 'block';
-        document.getElementById('reccuring-field').style.display = 'block';
-        document.getElementById('accessibility-field').style.display = 'block';
-        document.getElementById('description-field').style.display = 'block';
-    } else if (this.value == '2') { // errand
-        document.getElementById('departingFrom-field').style.display = 'block';
-        document.getElementById('departingAt-field').style.display = 'block';
-        document.getElementById('destination-field').style.display = 'block';
-        document.getElementById('stops-field').style.display = 'block';
-        document.getElementById('accessibility-field').style.display = 'block';
-        document.getElementById('description-field').style.display = 'block';
-    } else if (this.value == '3') { // leisure
-        document.getElementById('departingFrom-field').style.display = 'block';
-        document.getElementById('departingAt-field').style.display = 'block';
-        document.getElementById('destination-field').style.display = 'block';
-        document.getElementById('arrival-field').style.display = 'block';
-        document.getElementById('duration-field').style.display = 'block';
-        document.getElementById('accessibility-field').style.display = 'block';
-        document.getElementById('description-field').style.display = 'block';
-    }
-});
+function showCommuteFields() {
+    document.getElementById('departingFrom-field').style.display = 'block';
+    document.getElementById('departingAt-field').style.display = 'block';
+    document.getElementById('destination-field').style.display = 'block';
+    document.getElementById('arrival-field').style.display = 'block';
+    document.getElementById('reccuring-field').style.display = 'block';
+    document.getElementById('accessibility-field').style.display = 'block';
+    document.getElementById('description-field').style.display = 'block';
+}
+
+function showErrandFields() {
+    document.getElementById('departingFrom-field').style.display = 'block';
+    document.getElementById('departingAt-field').style.display = 'block';
+    document.getElementById('destination-field').style.display = 'block';
+    document.getElementById('stops-field').style.display = 'block';
+    document.getElementById('accessibility-field').style.display = 'block';
+    document.getElementById('description-field').style.display = 'block';
+}
+
+function showLeisureFields() {
+    document.getElementById('departingFrom-field').style.display = 'block';
+    document.getElementById('departingAt-field').style.display = 'block';
+    document.getElementById('destination-field').style.display = 'block';
+    document.getElementById('arrival-field').style.display = 'block';
+    document.getElementById('duration-field').style.display = 'block';
+    document.getElementById('accessibility-field').style.display = 'block';
+    document.getElementById('description-field').style.display = 'block';
+}
