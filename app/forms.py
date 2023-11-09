013aabf4fc809a2458f19f72841f88f0c45f5567
tmayer0 <mayert2@southernct.edu>
2023-11-09 11:16:41 -0500

    added required if

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,20 @@ from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired, Email , Length, EqualTo, ValidationError, RequiredIf
 from app.models import User
 
+class RequiredIf(DataRequired):
+    """validator which makes a field required if another field is set and has a truthy value."""
+
+    def __init__(self, other_field_name, *args, **kwargs):
+        self.other_field_name = other_field_name
+        super(RequiredIf, self).__init__(*args, **kwargs)
+
+    def __call__(self, form, field):
+        other_field = form._fields.get(self.other_field_name)
+        if other_field is None:
+            raise Exception('no field named "%s" in form' % self.other_field_name)
+        if bool(other_field.data):
+            super(RequiredIf, self).__call__(form, field)
+
 class LoginForm(FlaskForm):
      email = StringField('', validators=[DataRequired(), Email()], render_kw={"placeholder": "Email"})
      password = PasswordField('', validators=[DataRequired(), Length(min=8)], render_kw={"placeholder": "Password"})
