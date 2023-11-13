1c2af26daff4584a77b5b14729646a999ba5518a
tmayer0 <mayert2@southernct.edu>
2023-11-12 20:45:58 -0500

    removed overflow to hide modal

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,7 +1,9 @@
 document.querySelector('.profile-pic').addEventListener('click', function() {
+    document.body.classList.add('modal-open');
     document.getElementById('profile-modal').classList.add('open');
 });
 
 document.getElementById('close-modal').addEventListener('click', function() {
+    document.body.classList.remove('modal-open');
     document.getElementById('profile-modal').classList.remove('open');
 });
