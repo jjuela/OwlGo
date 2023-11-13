d39cc288d26d4bb0d7b267579fbb0a8d24a504db
tmayer0 <mayert2@southernct.edu>
2023-11-12 21:08:56 -0500

    close span

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -4,7 +4,7 @@ document.querySelector('.profile-pic').addEventListener('click', function() {
     document.getElementById('overlay').style.display = 'block';
 });
 
-document.getElementById('close-modal').addEventListener('click', function() {
+document.querySelector('.close').addEventListener('click', function() {
     document.body.classList.remove('modal-open');
     document.getElementById('profile-modal').classList.remove('open');
     document.getElementById('overlay').style.display = 'none';
